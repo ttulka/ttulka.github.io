7ca0576f0c94cadc1522de5bd6e6cb5ce6b48220
Tomas Tulka <tomas.tulka@gmail.com>
2020-07-17 07:09:45 +0200

    settings added

--- prelinks/bundle/prelinks.js
+++ prelinks/bundle/prelinks.js
@@ -2,19 +2,29 @@
     'use strict';
 
     class PreLinks {
-        constructor(document, loader, history) {
+        constructor(document, loader, history, progressMethods) {
             this.document = document;
             this.loader = loader;
             this.history = history;
             this.anchors = [];
 
+            const progressMethodId = this._progressMethodIdFromHead(document);
+            this.progressMethod = progressMethods.find(({ id }) => id === progressMethodId);
+
             this._onClickEvent = this._onClickEvent.bind(this);
             this._onMouseenterEvent = this._onMouseenterEvent.bind(this);
             this._onHistoryPoppedEvent = this._onHistoryPoppedEvent.bind(this);
-            
+
             console.debug('PreLinks constructed.');
         }
-        init(currentUrl) {
+        start(currentUrl) {
+            this.history.start(currentUrl);
+            this._init(currentUrl);
+        }
+        stop() {
+            this._destroy();
+        }
+        _init(link) {
             document.querySelectorAll('a').forEach(a => {
                 this.anchors.push(a);
                 a.addEventListener('click', this._onClickEvent);
@@ -22,12 +32,12 @@
             });
 
             this.history.addEventListener('popped', this._onHistoryPoppedEvent);
-            
-            this.loader.add(currentUrl, this.document);
-                    
+
+            this.loader.add(link, this.document);
+
             console.debug('Prelinks initialized.');
         }
-        destroy() {        
+        _destroy() {
             this.anchors.forEach(a => {
                 a.removeEventListener('click', this._onClickEvent);
                 a.removeEventListener('mouseenter', this._onMouseenterEvent);
@@ -37,40 +47,60 @@
 
             console.debug('Prelinks destroyed.');
         }
-        _showLink(link) {
+        showLink(link) {
+            this.showProgress();
             this.loader.show(link)
                 .then(_ => {
-                    this.destroy();
-                    this.init(link);
+                    this._destroy();
+                    this._init(link);
+                    this.hideProgress();
                 })
                 .catch(err => console.error('Cannot show the page.', link, err));
         }
-        _loadLink(link) {
+        loadLink(link) {
             this.loader.load(link)
                 .catch(err => console.error('Cannot load the page.', link, err));
         }
+        showProgress() {
+            if (this.progressMethod) {
+                this.progressMethod.show(this.document);
+            }
+        }
+        hideProgress() {
+            if (this.progressMethod) {
+                this.progressMethod.hide(this.document);
+            }
+        }
         _onClickEvent(e) {
-            e.preventDefault();
-            const link = e.target.href;
-            if (link) {            
-                console.debug('Link clicked', link);
-                this.history.push(link);
-                this._showLink(link);
+            if (e.target.getAttribute('data-prelink') !== 'false') {
+                e.preventDefault();
+                const link = e.target.href;
+                if (link) {
+                    console.debug('Link clicked', link);
+                    this.history.push(link);
+                    this.showLink(link);
+                }
             }
         }
         _onMouseenterEvent(e) {
-            const link = e.target.href;
-            if (link) {
-                console.debug('Link entered', link);
-                this._loadLink(link);
+            if (e.target.getAttribute('data-prelink') !== 'false') {
+                const link = e.target.href;
+                if (link) {
+                    console.debug('Link entered', link);
+                    this.loadLink(link);
+                }
             }
         }
         _onHistoryPoppedEvent(e) {
             const link = e.detail;
             if (link) {
                 console.debug('Link popped', link);
-                this._showLink(link);     
+                this.showLink(link);
             }
+        }
+        _progressMethodIdFromHead() {
+            const progressMeta = this.document.querySelector('head meta[name="prelinks-progress"]');
+            return progressMeta ? progressMeta.getAttribute('content') : 'none';
         }
     }
 
@@ -81,8 +111,8 @@
             this.loading = new Set();
             console.debug('PageLoader constructed.');
         }
-        async load(link) {
-            if (!this.loading.has(link) && !this.pages.has(link)) {
+        async load(link, force = false) {
+            if (force || !this.loading.has(link) && !this.pages.has(link)) {
                 this.loading.add(link);
 
                 const html = await fetch(link)
@@ -99,16 +129,21 @@
             if (!this.pages.has(link)) {
                 await this.load(link);
             }
+            if (this._forceLoad(this.pages.get(link))) {
+                await this.load(link, true);
+            }
             const page = this.pages.get(link).cloneNode(true);
             const body = page.querySelector('body');
             const head = page.querySelector('head');
 
             this.document.querySelector('head').innerHTML = head.innerHTML;
-                
+
             const pageBody = this.document.querySelector('body');
             pageBody.innerHTML = body.innerHTML;
 
             pageBody.querySelectorAll('script').forEach(s => s.parentNode.replaceChild(this._scriptElement(s), s));
+
+            
 
             console.debug('Shown', link);
         }
@@ -133,6 +168,10 @@
                 dest.setAttribute(name, value);
             }
         }
+        _forceLoad(page) {
+            const meta = page.querySelector('head meta[name="prelinks-cache-control"]');
+            return meta && meta.getAttribute('content') === 'no-cache';
+        }
     }
 
     class LinksHistory extends EventTarget {
@@ -151,33 +190,45 @@
         stop() {
             window.removeEventListener('popstate', this._onPopstateEvent);
         }
+        push(link) {
+            this.history.pushState(link, link, link);
+            console.debug('Link pushed to history', link);
+        }
         _onPopstateEvent(e) {
             if (e.state) {
                 this.dispatchEvent(new CustomEvent('popped', { detail: e.state }));
             }
         }
-        push(link) {
-            this.history.pushState(link, link, link);
-            console.debug('Link pushed to history', link);
+    }
+
+    class ProgressMethod {
+        constructor(id, styleId, styleValue) {
+            this.id = id;
+            this.styleId = styleId;
+            this.styleValue = styleValue;
+            this.origin = '';
+        }
+        show(document) {
+            this.origin = document.body.style[this.styleId];
+            document.body.style[this.styleId] = this.styleValue;
+        }
+        hide(document) {
+            document.body.style[this.styleId] = this.origin;
         }
     }
 
     (function () {
-        const history = new LinksHistory(
-            window,
-            window.history);
         const prelinks = new PreLinks(
             window.document,
             new PageLoader(window.document),
-            history);
+            new LinksHistory(
+                window,
+                window.history),
+            [new ProgressMethod('blur', 'filter', 'blur(1rem)')]);
 
-        prelinks.init(window.location.href);
-        history.start(window.location.href);
+        prelinks.start(window.location.href);
 
-        window.addEventListener('unload', e => {
-            prelinks.destroy();
-            history.stop();
-        });
+        window.addEventListener('unload', _ => prelinks.stop());
     })();
 
 }());
