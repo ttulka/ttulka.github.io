c251dd3d39967671f5ed887e15b614cb73f0b63e
Tomas Tulka <tomas.tulka@gmail.com>
2020-07-26 10:11:01 +0200

    bugfixes

--- prelinks/bundle/prelinks.js
+++ prelinks/bundle/prelinks.js
@@ -1,6 +1,241 @@
 (function () {
     'use strict';
 
+    class PreLinks {
+        constructor(document, cache, history, showPage, progressMethod) {
+            this.document = document;
+            this.cache = cache;
+            this.history = history;
+            this.showPage = showPage;
+            this.progressMethod = progressMethod;
+            this.anchors = [];
+
+            this._onClickEvent = this._onClickEvent.bind(this);
+            this._onMouseenterEvent = this._onMouseenterEvent.bind(this);
+            this._onHistoryPoppedEvent = this._onHistoryPoppedEvent.bind(this);
+
+            console.debug('PreLinks constructed.');
+        }
+        start(currentUrl) {
+            this.history.start(currentUrl);
+            this._init(currentUrl);
+        }
+        stop() {
+            this._destroy();
+        }
+        _init(link) {
+            this.document.querySelectorAll('a').forEach(a => {
+                this.anchors.push(a);
+                a.addEventListener('click', this._onClickEvent);
+                a.addEventListener('mouseenter', this._onMouseenterEvent);
+            });
+
+            this.history.addEventListener('popped', this._onHistoryPoppedEvent);
+
+            this.cache.put(link, this.document);
+
+            console.debug('Prelinks initialized.');
+        }
+        _destroy() {
+            this.anchors.forEach(a => {
+                a.removeEventListener('click', this._onClickEvent);
+                a.removeEventListener('mouseenter', this._onMouseenterEvent);
+            });
+
+            this.history.removeEventListener('popped', this._onHistoryPoppedEvent);
+
+            console.debug('Prelinks destroyed.');
+        }
+        showLink(link) {
+            this.showProgress();
+            this.cache.page(link)
+                .then(p => this.showPage.show(p))
+                .then(_ => {
+                    this._destroy();
+                    this._init(link);
+                    this.hideProgress();
+                })
+                .catch(err => console.error('Cannot show the page.', link, err));
+        }
+        loadLink(link, force = false) {
+            this.cache.load(link, force)
+                .catch(err => console.error('Cannot load the page.', link, err));
+        }
+        showProgress() {
+            if (this.progressMethod) {
+                this.progressMethod.show(this.document);
+            }
+        }
+        hideProgress() { 
+            if (this.progressMethod) {
+                this.progressMethod.hide(this.document);
+            }
+        }
+        _onClickEvent(e) {
+            if (e.target.getAttribute('data-prelinks') !== 'false') {
+                e.preventDefault();
+                const link = e.target.href;
+                if (link) {
+                    console.debug('Link clicked', link);
+                    this.history.push(link);
+                    this.showLink(link);
+                }
+            }
+        }
+        _onMouseenterEvent(e) {
+            if (e.target.getAttribute('data-prelinks') !== 'false') {
+                const link = e.target.href;
+                if (link) {
+                    console.debug('Link entered', link);
+                    const force = e.target.getAttribute('data-prelinks-cache') === 'false';
+                    this.loadLink(link, force);
+                }
+            }
+        }
+        _onHistoryPoppedEvent(e) {
+            const link = e.detail;
+            if (link) {
+                console.debug('Link popped', link);
+                this.showLink(link);
+            }
+        }
+    }
+
+    class PageCache extends EventTarget {
+        constructor(loadPageFn, limit, alwaysForce = false) {
+            super();
+            this.loadPageFn = loadPageFn;
+            this.cache = new LmitedPageCache(limit && limit > 1 ? limit : 10);
+            this.alwaysForce = !!alwaysForce;
+            this.loading = new Set();
+
+            console.debug('PageCache constructed.');
+        }
+        async load(link, force = false) {
+            let cache = this.cache;
+            let hit = false;
+            if (!this.loading.has(link)) {
+                if (force || this.alwaysForce || !cache.has(link)) {
+                    this.loading.add(link);
+                    try {
+                        const page = await this.loadPageFn(link);
+
+                        cache = cache.put(link, page);
+                        this.cache = cache;
+
+                        console.debug('Loaded', link);
+                    } finally {
+                        this.loading.delete(link);
+                    }
+                } else {
+                    hit = true;
+                }
+                this.dispatchEvent(new CustomEvent(
+                    'loaded', { detail: { link, page: cache.get(link) } }));
+            } else {
+                hit = true;
+            }
+            if (hit) {
+                cache.hit(link);
+            }
+        }
+        async page(link) {
+            const onPageLoaded = new Promise(resolve => {
+                const listener = ({ detail }) => {
+                    if (detail.link === link) {
+                        this.removeEventListener('loaded', listener);
+                        resolve(detail.page.cloneNode(true));
+                    }
+                };
+                this.addEventListener('loaded', listener, false);
+            });
+            await this.load(link);
+            return onPageLoaded;
+        }
+        put(link, document) {
+            const cache = this.cache;
+            if (!cache.has(link) && !this.loading.has(link)) {
+                this.cache = cache.put(link, document.cloneNode(true));
+                console.debug('Put into cache', link);
+            }
+        }
+    }
+
+    class LmitedPageCache {
+        constructor(limit, initCache = new Map()) {
+            this.limit = limit;
+            this.pages = initCache;
+        }
+        get(link) {
+            if (this.pages.has(link)) {
+                return this.pages.get(link).document;
+            }
+        }
+        put(link, document) {
+            const newCache = new LmitedPageCache(this.limit, this.pages);
+
+            const hits = newCache.pages.has(link) ? newCache.pages.get(link).hits + 1 : 1;
+            newCache.pages.set(link, { document, hits });
+
+            newCache._cleanup(link);
+            return newCache;
+        }
+        has(link) {
+            return this.pages.has(link);
+        }
+        hit(link) {
+            const page = this.pages.get(link);
+            if (page) {
+                page.hits++;
+            }
+        }
+        _cleanup(currentLink) {
+            if (this.pages.size > this.limit) {
+                const entries = this.pages.entries();
+                let toRemove;
+                let min = Number.MAX_VALUE;
+                for (let [link, page] of entries) {
+                    if (page.hits < min && link !== currentLink) {
+                        toRemove = link;
+                        min = page.hits;
+                    }
+                }
+                if (toRemove) {
+                    this.pages.delete(toRemove);
+
+                    console.debug('Removed from cache', toRemove);
+                }
+            }
+        }
+    }
+
+    class LinksHistory extends EventTarget {
+        constructor(window, history) {
+            super();
+            this.window = window;
+            this.history = history;
+
+            this._onPopstateEvent = this._onPopstateEvent.bind(this);
+        }
+        start(url) {
+            this.history.replaceState(url, url, url);
+
+            window.addEventListener('popstate', this._onPopstateEvent);
+        }
+        stop() {
+            window.removeEventListener('popstate', this._onPopstateEvent);
+        }
+        push(link) {
+            this.history.pushState(link, link, link);
+            console.debug('Link pushed to history', link);
+        }
+        _onPopstateEvent(e) {
+            if (e.state) {
+                this.dispatchEvent(new CustomEvent('popped', { detail: e.state }));
+            }
+        }
+    }
+
     class ShowPage {
         constructor(document) {
             this.document = document;
@@ -86,249 +321,6 @@
                     cur.name === name && cur.value === value), true);
     }
 
-    class PreLinks {
-        constructor(document, cache, history, progressMethod) {
-            this.document = document;
-            this.cache = cache;
-            this.history = history;
-            this.progressMethod = progressMethod;
-            this.showPage = new ShowPage(document);
-            this.anchors = [];
-
-            this._onClickEvent = this._onClickEvent.bind(this);
-            this._onMouseenterEvent = this._onMouseenterEvent.bind(this);
-            this._onHistoryPoppedEvent = this._onHistoryPoppedEvent.bind(this);
-
-            console.debug('PreLinks constructed.');
-        }
-        start(currentUrl) {
-            this.history.start(currentUrl);
-            this._init(currentUrl);
-        }
-        stop() {
-            this._destroy();
-        }
-        _init(link) {
-            document.querySelectorAll('a').forEach(a => {
-                this.anchors.push(a);
-                a.addEventListener('click', this._onClickEvent);
-                a.addEventListener('mouseenter', this._onMouseenterEvent);
-            });
-
-            this.history.addEventListener('popped', this._onHistoryPoppedEvent);
-
-            this.cache.put(link, this.document);
-
-            console.debug('Prelinks initialized.');
-        }
-        _destroy() {
-            this.anchors.forEach(a => {
-                a.removeEventListener('click', this._onClickEvent);
-                a.removeEventListener('mouseenter', this._onMouseenterEvent);
-            });
-
-            this.history.removeEventListener('popped', this._onHistoryPoppedEvent);
-
-            console.debug('Prelinks destroyed.');
-        }
-        showLink(link) {
-            this.showProgress();
-            this.cache.page(link)
-                .then(p => this.showPage.show(p))
-                .then(_ => {
-                    this._destroy();
-                    this._init(link);
-                    this.hideProgress();
-                })
-                .catch(err => console.error('Cannot show the page.', link, err));
-        }
-        loadLink(link, force = false) {
-            this.cache.load(link, force)
-                .catch(err => console.error('Cannot load the page.', link, err));
-        }
-        showProgress() {
-            if (this.progressMethod) {
-                this.progressMethod.show(this.document);
-            }
-        }
-        hideProgress() {
-            if (this.progressMethod) {
-                this.progressMethod.hide(this.document);
-            }
-        }
-        _onClickEvent(e) {
-            if (e.target.getAttribute('data-prelinks') !== 'false') {
-                e.preventDefault();
-                const link = e.target.href;
-                if (link) {
-                    console.debug('Link clicked', link);
-                    this.history.push(link);
-                    this.showLink(link);
-                }
-            }
-        }
-        _onMouseenterEvent(e) {
-            if (e.target.getAttribute('data-prelinks') !== 'false') {
-                const link = e.target.href;
-                if (link) {
-                    console.debug('Link entered', link);
-                    const force = e.target.getAttribute('data-prelinks-cache') === 'false';
-                    this.loadLink(link, force);
-                }
-            }
-        }
-        _onHistoryPoppedEvent(e) {
-            const link = e.detail;
-            if (link) {
-                console.debug('Link popped', link);
-                this.showLink(link);
-            }
-        }
-    }
-
-    class PageCache extends EventTarget {
-        constructor(limit, alwaysForce = false) {
-            super();
-            this.cache = new LmitedPageCache(limit && limit > 1 ? limit : 10);
-            this.alwaysForce = !!alwaysForce;
-            this.loading = new Set();
-
-            console.debug('PageCache constructed.');
-        }
-        async load(link, force = false) {
-            let cache = this.cache;
-            if (this.loading.has(link)) {
-                return cache;
-            }
-            if (force || this.alwaysForce || !cache.has(link)) {
-                this.loading.add(link);
-
-                const page = await htmlPage(link);
-
-                cache = cache.put(link, page);
-                this.cache = cache;
-
-                this.loading.delete(link);
-                this.dispatchEvent(new CustomEvent('loaded', { detail: { link, page } }));
-
-                console.debug('Loaded', link);
-
-            } else {
-                cache.hit(link);
-            }
-            return cache;
-        }
-        async page(link) {
-            const onPageLoaded = new Promise(resolve => {
-                const listener = ({ detail }) => {
-                    if (detail.link === link) {
-                        this.removeEventListener('loaded', listener);
-                        resolve(detail.page);
-                    }
-                };
-                this.addEventListener('loaded', listener, false);
-            });
-
-            if (this.loading.has(link)) {
-                return onPageLoaded;
-            }
-            const cache = this.cache;
-            if (!cache.has(link)) {
-                this.load(link);
-                return onPageLoaded;
-            }
-            return cache.get(link).cloneNode(true);
-        }
-        put(link, document) {
-            const cache = this.cache;
-            if (!cache.has(link)) {
-                this.cache = cache.put(link, document.cloneNode(true));
-                console.debug('Loaded', link);
-            }
-        }
-    }
-
-    class LmitedPageCache {
-        constructor(limit, initCache = new Map()) {
-            this.limit = limit;
-            this.pages = initCache;
-        }
-        get(link) {
-            if (this.pages.has(link)) {
-                return this.pages.get(link).document;
-            }
-        }
-        put(link, document) {
-            const newCache = new LmitedPageCache(this.limit, this.pages);
-
-            const hits = newCache.pages.has(link) ? newCache.pages.get(link).hits + 1 : 1;
-            newCache.pages.set(link, { document, hits });
-
-            newCache._cleanup(link);
-            return newCache;
-        }
-        has(link) {
-            return this.pages.has(link);
-        }
-        hit(link) {
-            const page = this.pages.get(link);
-            if (page) {
-                page.hits++;
-            }
-        }
-        _cleanup(currentLink) {
-            if (this.pages.size > this.limit) {
-                const entries = this.pages.entries();
-                let toRemove;
-                let min = Number.MAX_VALUE;
-                for (let [link, page] of entries) {
-                    if (page.hits < min && link !== currentLink) {
-                        toRemove = link;
-                        min = page.hits;
-                    }
-                }
-                if (toRemove) {
-                    this.pages.delete(toRemove);
-
-                    console.debug('Removed from cache', toRemove);
-                }
-            }
-        }
-    }
-
-    function htmlPage(link) {
-        return fetch(link)
-            .then(r => r.text())
-            .then(r => new DOMParser().parseFromString(r, 'text/html'))
-    }
-
-    class LinksHistory extends EventTarget {
-        constructor(window, history) {
-            super();
-            this.window = window;
-            this.history = history;
-
-            this._onPopstateEvent = this._onPopstateEvent.bind(this);
-        }
-        start(url) {
-            this.history.replaceState(url, url, url);
-
-            window.addEventListener('popstate', this._onPopstateEvent);
-        }
-        stop() {
-            window.removeEventListener('popstate', this._onPopstateEvent);
-        }
-        push(link) {
-            this.history.pushState(link, link, link);
-            console.debug('Link pushed to history', link);
-        }
-        _onPopstateEvent(e) {
-            if (e.state) {
-                this.dispatchEvent(new CustomEvent('popped', { detail: e.state }));
-            }
-        }
-    }
-
     class ProgressMethod {
         constructor(id, styleId, styleValue) {
             this.id = id;
@@ -347,17 +339,19 @@
 
     (function () {
         const progressMethods = [
-            new ProgressMethod('blur', 'filter', 'blur(1rem)')
+            new ProgressMethod('blur', 'filter', 'blur(.5rem)')
         ];
 
         const prelinks = new PreLinks(
             window.document,
             new PageCache(
+                htmlPage,
                 settingValue('cache-limit'),
                 settingValue('cache-control') === 'no-cache'),
             new LinksHistory(
                 window,
                 window.history),
+            new ShowPage(window.document),
             progressMethods.find(({ id }) => id === settingValue('progress', 'none'))
         );
 
@@ -368,6 +362,12 @@
         function settingValue(name, def = null) {
             const meta = window.document.querySelector(`head meta[name="prelinks-${name}"]`);
             return meta && meta.getAttribute('content') || def;
+        }
+
+        function htmlPage(link) {
+            return fetch(link)
+                .then(r => r.text())
+                .then(r => new DOMParser().parseFromString(r, 'text/html'))
         }
     })();
 
